f04da85b0a9b263617ac9a618e1cd2555ebdf7a8
Lauren Richert <lsrichert@gmail.com>
2018-08-28 13:09:01 -0500

    added instructions for exercise and terminal output

--- Program.cs
+++ Program.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿// EXERCISE 8 INSTRUCTIONS
+
+// Define a Dictionary that contains information about several members of your family. Use the following example as a template.
+
+// Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+// myFamily.Add("sister", new Dictionary<string, string>(){ 
+//     {"name", "Krista"},
+//     {"age", "42"}
+// });
+// Next, iterate over each item in myFamily and produce the following output.
+
+// Krista is my sister and is 42 years old
+
+
+using System;
 using System.Collections.Generic;
 
 namespace exercise8FamilyDictionary
@@ -35,3 +50,8 @@ Console.WriteLine($"{familyMember.Value["name"]} is my {familyMember.Key} and is
 }
 }
 
+// OUTPUT FROM TERMINAL:
+// Diane is my mom and is 65 years old
+// Clyde is my dad and is 67 years old
+// Jarod is my brother and is 37 years old
+// Christine is my wife and is 26 years old
